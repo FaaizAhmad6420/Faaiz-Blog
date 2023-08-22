7e4a6be1d37a04618d9c667201fa8fb66881bc8e
Faaiz Ahmad <faizahmad6420@gmail.com>
2023-08-22 16:36:10 +0500

    modified main.py

--- main.py
+++ main.py
@@ -12,19 +12,6 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import smtplib
 import os
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
@@ -271,14 +258,6 @@ def about():
     year = date.today().strftime("%Y")
     return render_template("about.html", year=year, current_user=current_user)
 
-
-# @app.route("/contact", methods=["GET", "POST"])
-# def contact():
-#     return render_template("contact.html", current_user=current_user)
-
-# Optional: You can include the email sending code from Day 60:
-# DON'T put your email and password here directly! The code will be visible when you upload to Github.
-# Use environment variables instead (Day 35)
 
 MAIL_ADDRESS = os.environ.get('EMAIL_KEY')
 MAIL_APP_PW = os.environ.get('PASSWORD_KEY')
